57671c574d29a02d04af6881d4e48e46e5a3e516
haffxu <haifeng.xu@iraytek.com>
2023-04-26 09:15:11 +0800

    fix(jenkins): 修复定时任务执行出错

--- jenkins/backup.sh
+++ jenkins/backup.sh
@@ -4,7 +4,6 @@ targetuser="targetuser"
 targetserver="1.1.1.1"
 targetdir="/backup/jenkins"
 
-rundir=$(cd "$(dirname "$0")";pwd)
 currentdate=$(date +%Y%m%d)
 
 function backup_tar {
@@ -15,23 +14,15 @@ function backup_tar {
         mkdir ../backup
     fi
 
-    tar -czvf ../backup/jenkins-backup-${currentdate}.tar.gz ./* --exclude-from=${rundir}/exclude.txt 
+    tar -czvf ../backup/jenkins-backup-${currentdate}.tar.gz ./* --exclude-from=${JENKINS_HOME}/../exclude.txt 
 }
-
-function test {
-    echo "Jenkins home dir: ${JENKINS_HOME}"
-    cd ${JENKINS_HOME}|| exit
-    echo "test"
-}
-
-cd ${rundir}
 
 ## tar backup file
 backup_tar
 #test
 
 # scp to bacup file
-scp  ../backup/jenkins-backup-${currentdate}.tar.gz ${targetuser}@${targetserver}:${targetdir}/backup
+scp  ../backup/jenkins-backup-${currentdate}.tar.gz ${targetuser}@${targetserver}:${targetdir}
 scp  ../jenkins.war ${targetuser}@${targetserver}:${targetdir}
 
 # clean temp file
