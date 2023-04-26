980f0b123542d7a1d362468d42305651dab0346e
haffxu <haifeng.xu@iraytek.com>
2023-04-26 09:21:30 +0800

    fix(sonarqube): 备份脚本执行出错

--- sonarqube/backup.sh
+++ sonarqube/backup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 # define target server,target dir and database account info
-targetuser="targetuser"
-targetserver="1.1.1.1"
-targetdir="/backup/sonarqube"
-sonaruser="user"
-sonarpwd="pwd"
+backup_user="targetuser"
+backup_server="1.1.1.1"
+backup_dir="/backup/sonarqube"
 
-# cd work dir
-workdir=$(cd "$(dirname "$0")";pwd) 
-cd ${workdir}
+soanr_dir="home/local/sonarqube"
+sonar_user="user"
+sonar_pwd="pwd"
+
+pg_dump="/home/local/pgsql/bin/pg_dumpl"
+
+# cd sonarqube dir
+cd ${soanr_dir}
 
 # create sql backup file
 currentdate=$(date +%Y%m%d) 
-pg_dump -U ${sonaruser}  -d ${sonarpwd} -Z 9 > sonarqube-data-${currentdate}
+${pg_dump} -U ${sonar_user}  -d ${sonar_pwd} -Z 9 > sonarqube-data-${currentdate}
 
 # scp file to bacup server
-scp -r extensions ${targetuser}@${targetserver}:${targetdir}
-scp  sonarqube-data-${currentdate} ${targetuser}@${targetserver}:${targetdir}
+scp -r extensions ${targetuser}@${backup_server}:${backup_dir}
+scp  sonarqube-data-${currentdate} ${targetuser}@${backup_server}:${backup_dir}
 
 # clean temp file
 rm  -f  sonarqube-data-${currentdate}
